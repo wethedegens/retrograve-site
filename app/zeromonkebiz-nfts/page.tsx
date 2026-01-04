091f040a578215b010067ab4ff5c41bcfc244be8
wethedegens <adamlohrbach@gmail.com>
2026-01-03 22:35:57 -0800

    Add ZeroMonkeBiz NFT page background image

--- app/zeromonkebiz-nfts/page.tsx
+++ app/zeromonkebiz-nfts/page.tsx
@@ -9,7 +9,7 @@ import NftGrid, { NFT } from "../components/NftGrid";
 const ZEROMONKEBIZ_COLLECTION_ID = "EwMMBSEiZVxbDnroRB7vPiCaF2GjGEmALWyUPProrVXY";
 
 // ✅ Use the background image that exists in /public (per your screenshot)
-const ZEROMONKEBIZ_BG_IMAGE = "/zeromonkebiz-bg.png";
+const ZEROMONKEBIZ_BG_IMAGE = "/zeromonkebiz-bg-nfts.png";
 
 export default function ZeroMonkeBizNftsPage() {
   const { publicKey, connected } = useWallet();
